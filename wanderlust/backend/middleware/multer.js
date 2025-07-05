8147b933c9d6ed078586908c6608d9d15a1fefd8
sanjay892000 <sanjay892000@gmail.com>
2025-07-06 00:01:20 +0530

    multer add

---
+++ wanderlust/backend/middleware/multer.js
@@ -0,0 +1,13 @@
+const multer = require('multer');
+const path = require('path')
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, '/public/uploads/');
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        cb(null, file.fieldname + '-' + Date.now() + ext);
+    }
+})
+const upload = multer({ storage: storage });
+module.exports = upload;
