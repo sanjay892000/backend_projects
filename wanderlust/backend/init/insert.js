1f351ed39338a1e8d1417196a184dfb12278e0a4
sanjay892000 <sanjay892000@gmail.com>
2025-07-13 01:56:54 +0530

    db change

--- wanderlust/backend/init/insert.js
+++ wanderlust/backend/init/insert.js
@@ -10,7 +10,7 @@ const URI = process.env.DB_URI
 
 const connectToDB= async()=>{
     try {
-        await mongoose.connect('mongodb+srv://SanjayAPI:g6WIo4g08DS0jDzd@sanjay.ecryqbg.mongodb.net/prabhat?retryWrites=true&w=majority')
+        await mongoose.connect(URI)
         console.log("MongoDB Connected to database ");
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error}`);
