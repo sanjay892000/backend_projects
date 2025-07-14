39d3fdeac94a38c8a0514e53a40f147725059d35
sanjay892000 <sanjay892000@gmail.com>
2025-07-15 01:24:41 +0530

    change multer path

--- wanderlust/backend/middleware/multer.js
+++ wanderlust/backend/middleware/multer.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path')
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'backend/public/');
+        cb(null, 'public/');
     },
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname)
