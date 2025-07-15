6fef9f60544941a189435b1fe90da57a6f8ff1ba
sanjay892000 <sanjay892000@gmail.com>
2025-07-16 00:58:36 +0530

    add public directory

--- wanderlust/src/components/Listing.jsx
+++ wanderlust/src/components/Listing.jsx
@@ -18,7 +18,7 @@ function Listing() {
   return (
     <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div class="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
-        {allListing.length > 0 ? allListing.map((element) => {
+        {allListing?.length > 0 ? allListing.map((element) => {
           return <ListingCard key={element._id}
             title={element.title}
             description={element.description}
