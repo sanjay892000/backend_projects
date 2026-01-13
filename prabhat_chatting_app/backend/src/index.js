feccb6c81f884fe8bd8b983d2ff83933b832dbb2
sanjay892000 <sanjay892000@gmail.com>
2026-01-13 15:43:16 +0530

    allow origin

--- prabhat_chatting_app/backend/src/index.js
+++ prabhat_chatting_app/backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://prabhatchatapp.onrender.com"],
+    origin: ["http://localhost:5173", "https://prabhatchatapp.netlify.app"],
     credentials: true,
   })
 );
