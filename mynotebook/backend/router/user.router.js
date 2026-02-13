8fca59d045ce1dfa8296afaef64b8c106c628f9e
sanjay892000 <sanjay892000@gmail.com>
2026-02-14 00:51:30 +0530

    user outer change

--- mynotebook/backend/router/user.router.js
+++ mynotebook/backend/router/user.router.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const authenticateUser = require("../middleware/authenticateUser");
-const { signupFunc, loginFunc, profilefunc, updatefunc, deletefunc } = require('../controllers/auth.controller')
+const { signupFunc,
+    loginFunc,
+    profileFunc,
+    updateFunc,
+    deleteFunc, } = require('../controllers/auth.controller')
 
 router.post("/signup", signupFunc);
 
 router.post("/login", loginFunc);
 
-router.get("/profile", authenticateUser, profilefunc);
+router.get("/profile", authenticateUser, profileFunc);
 
-router.put("/profile", authenticateUser, updatefunc);
+router.put("/profile", authenticateUser, updateFunc);
 
-router.delete("/profile", authenticateUser, deletefunc);
+router.delete("/profile", authenticateUser, deleteFunc);
 
 module.exports = router;
